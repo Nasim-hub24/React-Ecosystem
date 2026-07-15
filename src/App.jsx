79c54fc17f556bd7066905d28f8f9fd384334d26
Nasim-heb24 <ahmednasim2487@gmail.com>
2026-07-15 22:52:51 +0600

    your message here

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 
 import './App.css'
 import DaisyNav from './components/daisyNav/DaisyNav'
+import NavBar from './components/NavBar/NavBar'
 
 function App() {
   return (
     <>
       <div>
+        <NavBar></NavBar>
         <DaisyNav></DaisyNav>
         <h1 className='text-4xl'> VITE and REACT</h1>
       </div>
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,7 +2,17 @@
 const NavBar = () => {
     return (
         <div>
-            
+            <nav class=" flex justify-between mx-15">
+                <h3>NavBar</h3>
+                <ul class="flex">
+                    <li class="mr-10"> <a href="/home"></a> home </li>
+                    <li class="mr-10"><a href="/about"></a> About</li>
+                    <li class="mr-10"><a href="/signin">Sign In</a></li>
+                    <li class="mr-10"><a href="/blogs">Blogs</a></li>
+                </ul>
+                <button>sign in</button>
+            </nav>
+
         </div>
     );
 };
