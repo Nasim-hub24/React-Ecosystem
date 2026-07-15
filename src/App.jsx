7df852b27c8475ebed6f0d1161c40db02eb2d226
Nasim-heb24 <ahmednasim2487@gmail.com>
2026-07-15 22:13:37 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import DaisyNav from './components/daisyNav/DaisyNav'
+
+function App() {
+  return (
+    <>
+      <div>
+        <DaisyNav></DaisyNav>
+        <h1 className='text-4xl'> VITE and REACT</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
